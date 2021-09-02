18f98a1a9ebcab593d0154239b58ea079a135bec
guohongliang <guohongliang@jd.com>
2021-09-02 15:17:56 +0800

    init

---
+++ packages/demo-one/index.js
@@ -0,0 +1,11 @@
+
+// 导入组件，组件必须声明 name
+import demoOne from './src/demo-one'
+ 
+// 为组件提供 install 安装方法，供按需引入
+demoOne.install = function (Vue) {
+ Vue.component(demoOne.name, demoOne)
+}
+
+// 默认导出组件
+export default demoOne
